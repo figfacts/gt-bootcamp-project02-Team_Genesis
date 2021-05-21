16bd3a1a4e8c1d96517f8f4184c4791a16c3c01b
David Figueroa <figueroada@vcu.edu>
2021-05-21 17:17:21 -0400

    added comments with the heroku database name (commented out)

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- -----------------------------------------------------------------------------
 -- Script:   schema.sql
--- Purpose:  Drop and Create sportsMem_db.
+-- Purpose:  Drop and Create sportsMem_db or if using Heroku Drop and Create heroku_1c2b9ad6b23d211
 -- Input:    <none>   
 -- -----------------------------------------------------------------------------
 -- Author:   Mark Harrison
@@ -9,7 +9,9 @@
 
 -- DROP DATABASE
 DROP DATABASE IF EXISTS sportsMem_db;
+-- DROP DATABASE IF EXISTS heroku_1c2b9ad6b23d211;
 
 -- CREATE DATABASE
 CREATE DATABASE sportsMem_db;
+-- CREATE DATABASE heroku_1c2b9ad6b23d211;
 
