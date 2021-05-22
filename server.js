dfe384edb1448c80de1a6edf28d0d7823a7d9da5
David Figueroa <figueroada@vcu.edu>
2021-05-22 13:18:59 -0400

    updated line 5 models path; commented out line 26 html routes and lines 30-34 syncOptions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
+var db = require("./config/models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,16 +22,16 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/index.js")(app);
+// require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
