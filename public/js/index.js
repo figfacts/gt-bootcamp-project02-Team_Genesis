2ca4bfe4141e0b62b86059d94d17a8a01fa2fdff
Omari Grampus <ojgrampus@gmail.com>
2021-05-25 20:31:12 -0400

    new pull

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,58 @@
+//--------------------------------------------------------------------------
+//LOGIN BUTTONS
+//--------------------------------------------------------------------------
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 const loginModalBtn = document.getElementById('loginBtn')
 const closeLoginBtn = document.getElementById('loginCloseBtn')
-
+//--------------------------------------------------------------------------
+//SIGN-UP BUTTONS
+//--------------------------------------------------------------------------
 const myModalSignUp = document.getElementById('myModalSignUp')
 const myInputSignup = document.getElementById('myInputSignUp')
 const signUpModalBtn = document.getElementById('signUpBtn')
 const closeSignUpBtn = document.getElementById('signUpCloseBtn')
 
+//--------------------------------------------------------------------------
+//FOOTER "ABOUT-US" MODAL & BUTTONS
+//--------------------------------------------------------------------------
+const myModalAboutUs = document.getElementById('aboutUsModal')
+// const myInputSignup = document.getElementById('myInputSignUp')
+const aboutUsBtn = document.getElementById('aboutUsBtn')
+const closeAboutUsBtn = document.getElementById('aboutUsCloseBtn')
+
+//--------------------------------------------------------------------------
+//FOOTER CONTACT-US MODAL & BUTTONS
+//--------------------------------------------------------------------------
+const myModalContactUs= document.getElementById('contactUsModal')
+// const myInputSignup = document.getElementById('myInputSignUp')
+const contactUsBtn = document.getElementById('contactUsBtn')
+const closeContactUsBtn = document.getElementById('contactUsCloseBtn')
+
+
+
+
+
+
+//--------------------------------------------------------------------------
+//DROPDOWN BUTTON
+//--------------------------------------------------------------------------
 const dropDownBtn = document.getElementById('dropdownMenuLink')
 
+//--------------------------------------------------------------------------
+// DROPDOWN BUTTON FUNCTIONALITY
+//--------------------------------------------------------------------------
+function dropDownMenu() {
+  document.getElementById("dropDown").classList.toggle("show");
+}
 
+dropDownBtn.onclick = function () {
+  dropDownMenu()
+}
+
+//--------------------------------------------------------------------------
 // OPEN AND CLOSE LOGIN/SIGN-UP MODALS
-
+//--------------------------------------------------------------------------
 loginModalBtn.onclick = function() {
   myModal.style.display = "block";
 }
@@ -33,11 +73,29 @@ closeSignUpBtn.onclick = function(event) {
   }
 }
 
-// DROPDOWN BUTTON FUNCTIONALITY
-function dropDownMenu() {
-  document.getElementById("dropDown").classList.toggle("show");
+
+
+
+
+//--------------------------------------------------------------------------
+// OPEN AND CLOSE FOOTER ABOUT-US/CONTACT-US MODALS
+//--------------------------------------------------------------------------
+aboutUsBtn.onclick = function() {
+  myModalAboutUs.style.display = "block";
 }
 
-dropDownBtn.onclick = function () {
-  dropDownMenu()
+closeAboutUsBtn.onclick = function(event) {
+  if (event.target == closeAboutUsBtn) {
+    myModalAboutUs.style.display = "none";
+  }
+}
+
+signUpModalBtn.onclick = function() {
+  myModalSignUp.style.display = "block";
+}
+
+closeSignUpBtn.onclick = function(event) {
+  if (event.target === closeSignUpBtn) {
+    myModalSignUp.style.display = "none";
+  }
 }
