755478a20938805e5449377fda51149121353ff9
Justin Byrd <justin_byrd@hotmail.com>
2021-05-27 18:19:53 -0400

    Removed forwardAuthenticated function from config/auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,14 +1,8 @@
 module.exports = {
-	ensureAuthenticated: function(req, res, next) {
-	  if (req.isAuthenticated()) {
-		 return next();
-	  }
-	  res.redirect('/');
+	ensureAuthenticated: function (req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		res.redirect('/');
 	},
-	forwardAuthenticated: function(req, res, next) {
-	  if (!req.isAuthenticated()) {
-		 return next();
-	  }
-	  res.redirect('/login');      
-	}
- };
+};
