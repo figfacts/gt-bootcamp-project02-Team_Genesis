5bb2828e666831cecd32a10cd781fed79e9ac663
Justin Byrd <justin_byrd@hotmail.com>
2021-05-26 21:12:56 -0400

    Added route in routes/htmlRoutes.js for /profile

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,13 +16,8 @@ const router = require('express').Router();
       res.sendFile(path.join(__dirname, "../public/html/homepage.html"));
   });
 
-  // Load example page and pass in an example by id
-  router.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  router.get("/profile", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../public/html/profile.html"));
   });
 
   // Render 404 page for any unmatched routes
