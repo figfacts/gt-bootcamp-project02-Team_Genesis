549d45900171b817007a75d2cec3db85afb4ff08
David Figueroa <figueroada@vcu.edu>
2021-05-29 16:02:08 -0400

    removed parse passport

--- server.js
+++ server.js
@@ -41,13 +41,12 @@ app.use(
 	})
  );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());    // Is this needed or does express.json handle it?
+
 
 
 // Handlebars
