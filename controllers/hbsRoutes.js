576e4160e4ec0aaf5545397fadbff3057ca72614
Justin Byrd <justin_byrd@hotmail.com>
2021-05-30 15:46:11 -0400

    Removed some unused code from controllers/hbsRoutes.js

--- controllers/hbsRoutes.js
+++ controllers/hbsRoutes.js
@@ -1,15 +1,25 @@
 const router = require('express').Router();
 const { User } = require('../config/models');
 
+// -----------------------------------------------------------------------------
+// Get Homepage
+// -----------------------------------------------------------------------------
 router.get('/', async (req, res) => {
 	try {
 		const isAuthenticated = req.isAuthenticated();
-		console.log(`************* User authentication: ${isAuthenticated} ***********************`);
-		// console.log(`********************* req.session.passport.user= ${req.session.passport.user}`);
 		res.render('homePage', { 'isAuthenticated': isAuthenticated });
 	} catch(err) {
 		console.log(err);
 	}
 });
+
+// -----------------------------------------------------------------------------
+// Logout A User
+// -----------------------------------------------------------------------------
+router.get('/user/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+ });
+
 
 module.exports = router;
