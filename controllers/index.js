dde7f915e0c35f49c8569be5ba7de02bc61a8306
Asha Sanford <sanford.asha97@yahoo.com>
2021-05-31 15:21:04 -0400

    commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-
+const apiRoutes = require('../routes/api');
 const hbsRoutes = require('./hbsRoutes.js');
 
+
+router.use('/api', apiRoutes);
 router.use('/', hbsRoutes);
 
 module.exports = router;
